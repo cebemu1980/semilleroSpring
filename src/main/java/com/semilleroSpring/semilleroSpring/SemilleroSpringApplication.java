570369930746498947f52a15ed6391916e02d12d
cealbemu1980 <cebemu1980@gmail.com>
2021-11-19 15:29:40 -0500

    uno
    uno

---
+++ src/main/java/com/semilleroSpring/semilleroSpring/SemilleroSpringApplication.java
@@ -0,0 +1,13 @@
+package com.semilleroSpring.semilleroSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemilleroSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemilleroSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/semilleroSpring/semilleroSpring/SemilleroSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semilleroSpring.semilleroSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemilleroSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
